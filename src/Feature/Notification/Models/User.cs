4bfa13939b4cee8d7810f8cfdee9b3d8c1c90d01
shlcharolia <sohailcharolia@live.com>
2018-03-03 15:13:30 +0500

    User model updated

--- src/Feature/Notification/Models/User.cs
+++ src/Feature/Notification/Models/User.cs
@@ -12,5 +12,6 @@ namespace Notification.Models
         public string ProfileImageUrl { get; set; }
         public string DeviceType { get; set; }
         public string Group { get; set; }
+        public string Date { get; set; }
     }
 }
